34ad1eaf252e63681ea3c1feaeacd488f8c60f68
Neil Wen <jwen11@illinois.edu>
2016-08-22 14:46:39 -0500

    update run file

--- run.sh
+++ run.sh
@@ -4,6 +4,26 @@ sleep 1
 sudo ./consumer 1 
 sleep 1
 
+sudo ./producer 4 &
+sleep 1
+sudo ./consumer 4 
+sleep 1
+
+sudo ./producer 16 &
+sleep 1
+sudo ./consumer 16 
+sleep 1
+
+sudo ./producer 64 &
+sleep 1
+sudo ./consumer 64
+sleep 1
+
+sudo ./producer 256&
+sleep 1
+sudo ./consumer 256 
+sleep 1
+
 sudo ./producer 1024 &
 sleep 1
 sudo ./consumer 1024 
@@ -11,25 +31,5 @@ sleep 1
 
 sudo ./producer 4096 &
 sleep 1
-sudo ./consumer 4096 
-sleep 1
-
-sudo ./producer 8192 &
-sleep 1
-sudo ./consumer 8192
-sleep 1
-
-sudo ./producer 16384 &
-sleep 1
-sudo ./consumer 16384
-sleep 1
-
-sudo ./producer 32768 &
-sleep 1
-sudo ./consumer 32768
-sleep 1
-
-sudo ./producer 65536 &
-sleep 1
-sudo ./consumer 65536
+sudo ./consumer 4096
 sleep 1
