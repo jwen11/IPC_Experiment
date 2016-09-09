c7b6e57487b377c0925d9972462d5898ade27c59
Neil Wen <jwen11@illinois.edu>
2016-09-09 12:33:14 -0500

    update data collecting script, now sorted and a summary is generated when run collect.sh

--- log/util/merge.py
+++ log/util/merge.py
@@ -7,7 +7,7 @@ import os,sys
 col = 5 
 res = {}
 files = os.listdir('.')
-out = open("summary.log", 'w')
+out = open("summary.result", 'w')
 
 for file_name in files:
     if file_name[0:3] != 'log':
