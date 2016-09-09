a5864dfe4826863aab006469db8feb06a0838761
Neil Wen <jwen11@illinois.edu>
2016-09-09 12:32:09 -0500

    update data collecting script, now sorted and a summary is generated when run collect.sh

--- log/util/collect.sh
+++ log/util/collect.sh
@@ -1,10 +1,19 @@
 #! /bin/bash
 
+#compute the mean, 99th %, ... from the raw data
 for i in $(ls ..) ;
 do
     if [ ${i} != "util" ];
     then
         echo ${i}
         python ./collect_data.py ${i}
+        sort -n ${i}.result -o ${i}.result
     fi
 done
+
+#paste all the mean in a same file
+if [ -e "summary.txt" ];
+then
+    rm -f ./summary.txt
+fi
+python ./merge.py
